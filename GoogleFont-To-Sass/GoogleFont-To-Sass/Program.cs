9090a3e5bf202b6e0b176fc70577d5c1c44bdd8d
Josh <josh.hiles@stockport.gov.uk>
2017-07-20 14:08:00 +0100

    Josh - Now creates SCSS file with fonts from google import URL

--- GoogleFont-To-Sass/GoogleFont-To-Sass/Program.cs
+++ GoogleFont-To-Sass/GoogleFont-To-Sass/Program.cs
@@ -5,19 +5,30 @@ namespace GoogleFont_To_Sass
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string input = @"@import url('https://fonts.googleapis.com/css?family=Lato:900|Roboto:400,500i,700');";
+            //  @import url('https://fonts.googleapis.com/css?family=Lato:900|Roboto:400,500i,700');
+            //  C:\Users\[USER]\Desktop\_typography.scss
 
-            GetFontName(input);
+            Console.WriteLine("Please input google fonts import URL:");
+            var input = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Please input destination to save file e.g. c:\\example.scss");
+            var filePath = Console.ReadLine();
+
+            var fonts = GetFontName(input);
+            var sass = GenerateSass(fonts);
+            SaveSassFile(sass, filePath);
         }
 
-        public static void GetFontName(string input)
+        public static List<Font> GetFontName(string input)
         {
             var trimmedStart = input.Substring(input.IndexOf("=") + 1);
             var trimmedEnd = trimmedStart.Substring(0, trimmedStart.LastIndexOf(")") - 1);
 
             string[] splitted = trimmedEnd.Split('|');
+
+            List<Font> fonts = new List<Font>();
 
             foreach (var split in splitted)
             {
@@ -27,29 +38,45 @@ namespace GoogleFont_To_Sass
                 string weights = splittedWeightAndFont[1];
                 string[] weight = weights.Split(',');
 
-                Font finalFont = new Font();
-                finalFont.FontName = font;
+                Font finalFont = new Font
+                {
+                    FontName = font
+                };
 
                 foreach (var weightItem in weight)
                 {
                     finalFont.FontWeights.Add(weightItem);
                 }
-            }
 
+                fonts.Add(finalFont);
+            }
+            return fonts;
         }
 
-        public static void GenerateSass(int number, string name, string[] weight)
+        public static string GenerateSass(List<Font> fonts)
         {
-            string variableFontName = $"$Font-{number}";
-            string variableFont = $"$Font-{number}:'{name}';";
+            int number = 1;
+            string sassToWrite = "";
+            foreach (var font in fonts)
+            {
+                sassToWrite += $"$Font-{number}:{font.FontName};";
+                sassToWrite += "\r\n";
+                foreach (var weight in font.FontWeights)
+                {
+                    sassToWrite += $"$Font-{number}-{weight}:{weight};";
+                    sassToWrite += "\r\n";
+                    sassToWrite += $"@mixin Mixin-Font-{number}-{weight}{{\r\nfont-family:{font.FontName};\r\nfont-weight:{weight};\r\n}}";
+                    sassToWrite += "\r\n";
+                }
+                sassToWrite += "\r\n";
+                number++;
+            }
+            return sassToWrite;
+        }
 
-            string variableWeightName = $"$Font-{number}-{weight}";
-            string variableWeight = $"$Font-{number}-{weight}:{weight}';";
-
-            string mixin = @"@mixin Mixin-Font" + number + "-" + weight + "{" +
-                           "font-family:" + variableFontName + ";" +
-                           "font-weight:" + variableWeightName + ";"
-                           + "}";
+        public static void SaveSassFile(string inputSass, string filePath)
+        {
+            System.IO.File.WriteAllText(filePath, inputSass);
         }
 
         public class Font
@@ -57,6 +84,12 @@ namespace GoogleFont_To_Sass
             public string FontName { get; set; }
 
             public List<string> FontWeights { get; set; }
+
+            public Font()
+            {
+                FontName = FontName;
+                FontWeights = new List<string>();
+            }
         }
     }
 }
